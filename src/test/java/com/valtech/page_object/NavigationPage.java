1f48197973ed6a91d5e51aff0f40156dfe15da4c
Harsha691 <harshanagpudi@gmail.com>
2018-10-04 22:54:05 +0100

    Implementation of Page objects

---
+++ src/test/java/com/valtech/page_object/NavigationPage.java
@@ -0,0 +1,16 @@
+package com.valtech.page_object;
+
+import com.valtech.drivers.DriverFactory;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+
+public class NavigationPage extends DriverFactory {
+
+
+    @FindBy(css = "h1")
+    private WebElement title;
+
+    public String getPageTitle() {
+        return title.getText();
+    }
+}
